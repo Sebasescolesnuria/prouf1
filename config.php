d09c5950e0fa707a02d449e14ec34863c0f8dade
root <root@debian10.4>
2020-11-08 00:06:01 +0100

    Projecte 1 Sebas

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     $dbhost = "127.0.0.1";
-    $dbname = "prouf1";
-    $dbuser = "prouf1";
+    $dbname = "NoSe_prouf1";
+    $dbuser = "NoSe_prouf1";
     $dbpass = "linuxlinux";
     $dsn = 'mysql:host='.$dbhost.';dbname='.$dbname.';';
     $driver = 'mysql';
